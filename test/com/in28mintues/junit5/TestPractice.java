e85dcfc794163112165c96ecf794b0f207356d7f
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-08 22:07:18 +0900

    新規テストの実装

--- test/com/in28mintues/junit5/TestPractice.java
+++ test/com/in28mintues/junit5/TestPractice.java
@@ -1,8 +1,12 @@
 package com.in28mintues.junit5;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class TestPractice {
@@ -12,8 +16,19 @@ class TestPractice {
 	}
 
 	@Test
-	void test() {
-		
+	@RepeatedTest(10)
+	void contains_basic() {
+		assertFalse("abcdefgh".contains("ijk"));
 	}
-
+	
+	@Test
+	void performanceTest() {
+		assertTimeout(Duration.ofSeconds(5), () -> {
+			for(int i = 0; i <=10000; i++) {
+				int j = i;			
+				System.out.println(j);
+			}		
+		}
+	  );
+	}
 }
