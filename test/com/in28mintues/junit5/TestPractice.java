4e8f88c5952e97089647ab47ba5b66b664a4c3e5
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-08 21:53:40 +0900

    新規クラスの作成

---
+++ test/com/in28mintues/junit5/TestPractice.java
@@ -0,0 +1,19 @@
+package com.in28mintues.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+class TestPractice {
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		
+	}
+
+}
